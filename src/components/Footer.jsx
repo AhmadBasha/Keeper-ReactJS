14955d26e63c7e0fb6e251f416f7de6b48e597c9
AhmadBasha <a.ahmadbasha@hotmail.com>
2020-08-16 23:31:16 +1000

    Complete Keeper project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const date = new Date();
+const currentTime = date.getFullYear();
+
+function Footer() {
+    return (
+      <footer>
+           <p>Copyright ⓒ{currentTime} </p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
